e49d14c4fdb0eca6817c23962aca67bc6c573f5d
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2018-04-19 12:32:56 +0200

    Typo.

--- scripts/test_example.sh
+++ scripts/test_example.sh
@@ -1,6 +1,6 @@
 # Commands to test pretrained models.
 cd src
-python3 batch_test.py --model FormulNet-basic --log ../logs/test_formulanet_basic.log --compatible
-python3 batch_test.py --model FormulNet-basic-uc --log ../logs/test_formulanet_basic_uc.log --compatible
-python3 batch_test.py --model FormulNet --log ../logs/test_formulanet.log
-python3 batch_test.py --model FormulNet-uc --log ../logs/test_formulanet_uc.log
+python3 batch_test.py --model FormulaNet-basic --log ../logs/test_formulanet_basic.log --compatible
+python3 batch_test.py --model FormulaNet-basic-uc --log ../logs/test_formulanet_basic_uc.log --compatible
+python3 batch_test.py --model FormulaNet --log ../logs/test_formulanet.log
+python3 batch_test.py --model FormulaNet-uc --log ../logs/test_formulanet_uc.log
